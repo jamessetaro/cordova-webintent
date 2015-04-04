c38d2f17e3ae210d0abcaad2d5942eb4f09db0bc
James Setaro <james.setaro@gmail.com>
2015-04-04 17:27:39 -0500

    update activity

--- src/android/WebIntent.java
+++ src/android/WebIntent.java
@@ -213,7 +213,8 @@ public class WebIntent extends CordovaPlugin {
 
     void startActivityForResult(String action, Uri uri, String type, Map<String, String> extras) {
         Intent i = buildActivity(action, uri, type, extras);
-        ((CordovaActivity)this.cordova.getActivity()).startActivityForResult(i);
+        Activity activity = this.cordova.getActivity();
+        activity.startActivityForResult(shareIntent, 1);
     }
 
     void sendBroadcast(String action, Map<String, String> extras) {
